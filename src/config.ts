f5633ece292cc8f2f964317c2e767e4f3e47a54f
ztlemceny <ztlemceny@gmail.com>
2026-04-19 17:52:05 +0100

    updating booking url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   name: 'Ztlemceny',
-  bookingUrl: 'https://your-reservation-app.com',
+  bookingUrl: 'https://calendly.com/ztlemceny/new-meeting',
   whatsappNumber: '+213 656030566',
   whatsappUrl: 'https://wa.me/213656030566',
   email: 'ztlemceny@gmail.com',

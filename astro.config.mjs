4649697210b495c5c3fb6ad8eda9fb3372a5f1e0
ztlemceny <ztlemceny@gmail.com>
2026-04-25 01:08:53 +0100

    changed host

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://ztlemceny.github.io',
+  base: '/booking_personal',
   integrations: [tailwind()],
   output: 'static',
 });
